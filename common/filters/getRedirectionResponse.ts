e34a70a02bfb5a8ca49768206d14304d8c396a3d
오동엽 <dnlwllms@hdc-labs.com>
2024-09-02 16:56:19 +0900

    update

--- common/filters/getRedirectionResponse.ts
+++ common/filters/getRedirectionResponse.ts
@@ -13,6 +13,20 @@ export default function getRedirectionResponse(request: NextRequest) {
       request.nextUrl.port = "8080";
       return NextResponse.redirect(new URL(request.nextUrl));
     }
+  } else {
+    if (
+      request.nextUrl.hostname === "smartr-front-test-desktop.vercel.app" &&
+      device.type === "mobile"
+    ) {
+      request.nextUrl.hostname = "smartr-front-test-mobile.vercel.app";
+      return NextResponse.redirect(new URL(request.nextUrl));
+    } else if (
+      request.nextUrl.hostname === "smartr-front-test-mobile.vercel.app" &&
+      device.type !== "mobile"
+    ) {
+      request.nextUrl.hostname = "smartr-front-test-desktop.vercel.app";
+      return NextResponse.redirect(new URL(request.nextUrl));
+    }
   }
 
   return NextResponse.next();
